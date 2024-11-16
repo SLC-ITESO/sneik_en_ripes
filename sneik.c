13a4d67edba579a9b9d27c43c146b332a4959eae
SantiL <ismael.lopez@iteso.mx>
2024-11-15 19:45:40 -0600

    Ya se mueve como al principio
    Solo el cuadradito

--- sneik.c
+++ sneik.c
@@ -7,8 +7,12 @@ void set_pixel(unsigned int x, unsigned int y, unsigned int color);
 void delay();
 void spawn_square(unsigned int x, unsigned int y, unsigned int color);
 void spawn_apple();
+
+void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy, unsigned int color);
 void spawn_snake();
-void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy);
+void grow_snake();
+void move_snake();
+void generate_snake();
 
 volatile unsigned int * d_pad_u = D_PAD_0_UP;
 volatile unsigned int * d_pad_d = D_PAD_0_DOWN;
@@ -55,48 +59,26 @@ void main (){
       
     spawn_apple();
     spawn_snake();
-    
+    generate_snake();
     
     while (1){
-        if (*d_pad_u == 1 && mov != 2) {
-            mov = 1;
+        
+        generate_snake();
+        
+        if (*d_pad_u == 1 && head -> dir != 2) {
+            head -> dir = 1;
         }
-        if (*d_pad_d == 1 && mov != 1) {
-            mov = 2;
+        if (*d_pad_d == 1 && head -> dir != 1) {
+            head -> dir = 2;
         }
-        if (*d_pad_l == 1 && mov != 4) {
-            mov = 3;
+        if (*d_pad_l == 1 && head -> dir != 4) {
+            head -> dir = 3;
         }
-        if (*d_pad_r == 1 && mov != 3) {
-            mov = 4;
+        if (*d_pad_r == 1 && head -> dir != 3) {
+            head -> dir = 4;
         }
-        //movimiento(mov);
+        move_snake();
         delay();
-    }
-}
-
-void movimiento(unsigned int mov){
-    switch(mov){
-        case 1://arriba
-            spawn_square(x,y,0);
-            y += 1;
-            spawn_square(x,y,ro);
-            break;
-        case 2://abajo
-            spawn_square(x,y,0);
-            y -= 1;
-            spawn_square(x,y,ro);
-            break;
-        case 3://izq
-            spawn_square(x,y,0);
-            x -= 1;
-            spawn_square(x,y,ro);
-            break;
-        case 4://derecha
-            spawn_square(x,y,0);
-            x += 1;
-            spawn_square(x,y,ro);
-            break;
     }
 }
 
@@ -114,7 +96,7 @@ void spawn_snake(){
     head = new;
     tail = new;
     
-    spawn_snake_part(new->x,new->y,new->xx,new->yy);
+    //spawn_snake_part(new->x,new->y,new->xx,new->yy,ro);
     //spawn_snake_part(new->x+2,new->y,new->xx+2,new->yy);
     return;
 }
@@ -141,74 +123,86 @@ void grow_snake(){
             new->yy = tail->xx - 2;
             break;
     }
-    new->next = NULL;
-    tail->next = new;
+    new -> next = NULL;
+    tail -> next = new;
 
     tail = new;
 }
 
 
-void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy){
-    unsigned int color = 0x00ffff00;
+void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy, unsigned int color) {
     set_pixel(x, y, color);
     set_pixel(xx, y, color);
     set_pixel(x, yy, color);
     set_pixel(xx, yy, color);
-    return;
 }
 
-void move_snake(){
-    unsigned short mov_x = head -> x;
-    unsigned short mov_y = head -> y;
-    unsigned short mov_xx = head -> xx;
-    unsigned short mov_yy = head -> yy;
-    unsigned short mov_dir = head -> dir;
-    
-    // 1 = arriba, 2 = abajo, 3 = izq, 4 = derecha
-    switch(head->dir) {
-        case 1:
+void move_snake() {
+    // Limpiar la posición anterior de la cola
+    Snake *temp = tail;
+    spawn_snake_part(temp->x, temp->y, temp->xx, temp->yy, 0x00000000);
+
+
+    // Mover la cabeza
+    unsigned short prev_x = head->x;
+    unsigned short prev_y = head->y;
+    unsigned short prev_xx = head->xx;
+    unsigned short prev_yy = head->yy;
+
+    switch (head->dir) {
+        case 1: // Arriba
             head->y++;
+            head->yy++;
             break;
-        case 2:
+        case 2: // Abajo
             head->y--;
+            head->yy--;
             break;
-        case 3:
+        case 3: // Izquierda
             head->x--;
+            head->xx--;
             break;
-        case 4:
+        case 4: // Derecha
             head->x++;
+            head->xx++;
             break;
     }
-    
-    Snake *temp = head;
-    if(temp ->next != NULL) {
-        temp  = temp -> next;
+
+    // Mover el resto del cuerpo
+    temp = head->next;
+    while (temp != NULL) {
+        unsigned short temp_x = temp->x;
+        unsigned short temp_y = temp->y;
+        unsigned short temp_xx = temp->xx;
+        unsigned short temp_yy = temp->yy;
+
+        temp->x = prev_x;
+        temp->y = prev_y;
+        temp->xx = prev_xx;
+        temp->yy = prev_yy;
+
+        prev_x = temp_x;
+        prev_y = temp_y;
+        prev_xx = temp_xx;
+        prev_yy = temp_yy;
+
+        temp = temp->next;
     }
-     while(temp  != NULL) {
-        
-        int new_x = temp ->x;
-        int new_y = temp ->y;
-        int new_dir = temp ->dir;
 
-        temp -> x = mov_x;
-        temp -> y = mov_y;
-        temp -> dir = mov_dir;
+    // Dibujar la serpiente en sus nuevas posiciones
+    temp = head;
+    spawn_snake_part(temp->x, temp->y, temp->xx, temp->yy, ro);
 
-        temp = temp -> next;
-
-        mov_x = new_x;
-        mov_y = new_y;
-        mov_dir = new_dir;
-
-    }
-    return;
 }
 
-void spawn_square(unsigned int x, unsigned int y, unsigned int color){
-    set_pixel(x, y, color);
-    set_pixel(x, y+1, color);
-    set_pixel(x+1, y, color);
-    set_pixel(x+1, y+1, color);
+void generate_snake(){
+    Snake *gen = head;
+    while(gen != NULL){
+        spawn_snake_part(gen->x,gen->y,gen->xx,gen->yy,ro);
+        gen = gen -> next;
+    }
+    return;
+
 }
 
 void spawn_apple(){
@@ -240,3 +234,33 @@ void delay() {
     for (volatile int i = 0; i < 500; i++) {}
 }
 
+void spawn_square(unsigned int x, unsigned int y, unsigned int color){
+    set_pixel(x, y, color);
+    set_pixel(x, y+1, color);
+    set_pixel(x+1, y, color);
+    set_pixel(x+1, y+1, color);
+}
+void movimiento(unsigned int mov){
+    switch(mov){
+        case 1://arriba
+            spawn_square(x,y,0);
+            y += 1;
+            spawn_square(x,y,ro);
+            break;
+        case 2://abajo
+            spawn_square(x,y,0);
+            y -= 1;
+            spawn_square(x,y,ro);
+            break;
+        case 3://izq
+            spawn_square(x,y,0);
+            x -= 1;
+            spawn_square(x,y,ro);
+            break;
+        case 4://derecha
+            spawn_square(x,y,0);
+            x += 1;
+            spawn_square(x,y,ro);
+            break;
+    }
+}
