1a2cbc4ddcdce46c397c2e72e39696fc73e412ee
SantiL <ismael.lopez@iteso.mx>
2024-11-16 21:12:10 -0600

    Funciona comer_apple de manera parcial
    Solo se le suman un par de leds, y creo que tail no se updatea

--- sneik.c
+++ sneik.c
@@ -2,11 +2,11 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 
-
 void set_pixel(unsigned int x, unsigned int y, unsigned int color);
 void delay();
-void spawn_square(unsigned int x, unsigned int y, unsigned int color);
 void spawn_apple();
+void despawn_apple();
+void come_apple();
 
 void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy, unsigned int color);
 void spawn_snake();
@@ -14,121 +14,159 @@ void grow_snake();
 void move_snake();
 void generate_snake();
 
-volatile unsigned int * d_pad_u = D_PAD_0_UP;
-volatile unsigned int * d_pad_d = D_PAD_0_DOWN;
-volatile unsigned int * d_pad_l = D_PAD_0_LEFT;
-volatile unsigned int * d_pad_r = D_PAD_0_RIGHT;
+volatile unsigned int *d_pad_u = D_PAD_0_UP;
+volatile unsigned int *d_pad_d = D_PAD_0_DOWN;
+volatile unsigned int *d_pad_l = D_PAD_0_LEFT;
+volatile unsigned int *d_pad_r = D_PAD_0_RIGHT;
 
-//led max
+// cosos del tablero
 unsigned short max_x = LED_MATRIX_0_WIDTH - 1;
 unsigned short max_y = LED_MATRIX_0_HEIGHT - 1;
 
-//apple coords
+//cosos del manzano
 unsigned int ax = 0;
 unsigned int ay = 0;
+unsigned int axx = 0;
+unsigned int ayy = 0;
+// Culebra
+#define MAX_SNAKE_PARTS 20
 
-//snake coords
-unsigned int x = 17;
-unsigned int y = 12;
-
-//snake stuffs
-struct snake {
-    
-    unsigned short x;
-    unsigned short xx;
-    unsigned short y;
-    unsigned short yy;
+typedef struct {
+    unsigned short x, xx;
+    unsigned short y, yy;
     unsigned short dir;
+} SnakePart;
 
-    struct snake *next;
+SnakePart snake[MAX_SNAKE_PARTS];
+int snake_size = 1; // tamaño de la serpiente
 
-};
-typedef struct snake Snake;
-
-Snake *head;
-Snake *tail;
-
-//color
+// colores:
 unsigned int ro = 0x00ff0000;
+unsigned int ve = 0x0000ff00;
 
-void main (){
-
-    //spawn_square(x,y,ro);
+void main() {
+    unsigned int mov = 4; // 1 = , 2 = down, 3 = left, 4 = right
     
-    unsigned int mov = 4;    // 1 = arriba, 2 = abajo, 3 = izq, 4 = derecha
-      
-    spawn_apple();
+    // spawnea todo
+    spawn_apple(ve);
     spawn_snake();
-    generate_snake();
     
-    while (1){
-        
+    while (1) {
+        //spawn_apple(ve);
         generate_snake();
         
-        if (*d_pad_u == 1 && head -> dir != 2) {
-            head -> dir = 1;
+        // DPAD
+        if (*d_pad_u == 1 && snake[0].dir != 2) {
+            snake[0].dir = 1;
         }
-        if (*d_pad_d == 1 && head -> dir != 1) {
-            head -> dir = 2;
+        if (*d_pad_d == 1 && snake[0].dir != 1) {
+            snake[0].dir = 2;
         }
-        if (*d_pad_l == 1 && head -> dir != 4) {
-            head -> dir = 3;
+        if (*d_pad_l == 1 && snake[0].dir != 4) {
+            snake[0].dir = 3;
         }
-        if (*d_pad_r == 1 && head -> dir != 3) {
-            head -> dir = 4;
+        if (*d_pad_r == 1 && snake[0].dir != 3) {
+            snake[0].dir = 4;
         }
+
+        //Movimiento de la culebra
         move_snake();
+        come_apple();
         delay();
     }
 }
 
-void spawn_snake(){
-    Snake *new = malloc(sizeof(Snake));
-    new->x = rand() % (max_x - 8); 
-    new->y = rand() % (max_y - 4); 
-    new->xx = new->x-1;
-    new->yy = new->y-1;
-    
-    // 1 = arriba, 2 = abajo, 3 = izq, 4 = derecha
-    new->dir = 4;
-    new->next = NULL;
-    
-    head = new;
-    tail = new;
-    
-    //spawn_snake_part(new->x,new->y,new->xx,new->yy,ro);
-    //spawn_snake_part(new->x+2,new->y,new->xx+2,new->yy);
+void spawn_snake() {
+    if (snake_size < MAX_SNAKE_PARTS) {
+        SnakePart *head = &snake[0];
+        snake[0].x = rand() % (max_x - 8);
+        snake[0].y = rand() % (max_y - 4);
+        snake[0].xx = snake[0].x + 1;
+        snake[0].yy = snake[0].y + 1;
+        snake[0].dir = 4; // Comienza moviendose a la derecha
+        snake_size = 1;
+    }
+}
+void crece_snake(){
+    snake_size++;
     return;
 }
-
-void grow_snake(){
-    Snake *new = malloc(sizeof(Snake));
-    
-    // 1 = arriba, 2 = abajo, 3 = izq, 4 = derecha
-    switch(tail->dir) {
-        case 1:
-            new->y = tail->y - 2;
-            new->yy = tail->y - 2;
-            break;
-        case 2:
-            new->y = tail->y + 2;
-            new->yy = tail->y + 2;
-            break;
-        case 3:
-            new->y = tail->x + 2;
-            new->yy = tail->xx + 2;
-            break;
-        case 4:
-            new->y = tail->x - 2;
-            new->yy = tail->xx - 2;
-            break;
-    }
-    new -> next = NULL;
-    tail -> next = new;
-
-    tail = new;
+void come_apple(){
+if ((snake[0].x == ax && snake[0].y == ay) ||
+    (snake[0].x == axx && snake[0].y == ay) ||
+    (snake[0].x == ax && snake[0].y == ayy) ||
+    (snake[0].x == axx && snake[0].y == ayy) ||
+    (snake[0].xx == ax && snake[0].yy == ay) ||
+    (snake[0].xx == axx && snake[0].yy == ay) ||
+    (snake[0].xx == ax && snake[0].yy == ayy) ||
+    (snake[0].xx == axx && snake[0].yy == ayy)) {
+         despawn_apple();
+         crece_snake();
+         spawn_apple(ve);
+     }
+     else{
+         return;
+     }
 }
 
+void move_snake() {
+    if (snake_size <= 0) return;
+
+    // Se elimina la cola para que se vea movimiento
+    SnakePart *tail = &snake[snake_size - 1];
+    spawn_snake_part(tail->x, tail->y, tail->xx, tail->yy, 0x00000000);
+
+    // Actualiza las posiciones de las partes, desde la cola
+    for (int i = snake_size - 1; i > 0; i--) {
+        snake[i] = snake[i - 1];
+    }
+
+    // Mueve la cabeza { snake[0] es la cabeza}
+    SnakePart *head = &snake[0];
+    switch (head->dir) {
+        case 1: // Up
+            head->y++;
+            head->yy++;
+            break;
+        case 2: // Down
+            head->y--;
+            head->yy--;
+            break;
+        case 3: // Left
+            head->x--;
+            head->xx--;
+            break;
+        case 4: // Right
+            head->x++;
+            head->xx++;
+            break;
+    }
+    //Dinuja la nueva cabeza
+    spawn_snake_part(head->x, head->y, head->xx, head->yy, ro);
+}
+
+void generate_snake() {
+    for (int i = 0; i < snake_size; i++) {
+        spawn_snake_part(snake[i].x, snake[i].y, snake[i].xx, snake[i].yy, ro);
+    }
+}
+void despawn_apple(){
+    set_pixel(ax, ay, 0);
+    set_pixel(axx, ay, 0);
+    set_pixel(ax, ayy, 0);
+    set_pixel(axx, ayy, 0);
+}
+void spawn_apple(unsigned int color_m) {
+    ax = rand() % (max_x + 1);
+    ay = rand() % (max_y + 1);
+    axx = ax - 1;
+    ayy = ay -1;
+    
+    set_pixel(ax, ay, color_m);
+    set_pixel(axx, ay, color_m);
+    set_pixel(ax, ayy, color_m);
+    set_pixel(axx, ayy, color_m);
+}
 
 void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy, unsigned int color) {
     set_pixel(x, y, color);
@@ -137,130 +175,15 @@ void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, uns
     set_pixel(xx, yy, color);
 }
 
-void move_snake() {
-    // Limpiar la posición anterior de la cola
-    Snake *temp = tail;
-    spawn_snake_part(temp->x, temp->y, temp->xx, temp->yy, 0x00000000);
-
-
-    // Mover la cabeza
-    unsigned short prev_x = head->x;
-    unsigned short prev_y = head->y;
-    unsigned short prev_xx = head->xx;
-    unsigned short prev_yy = head->yy;
-
-    switch (head->dir) {
-        case 1: // Arriba
-            head->y++;
-            head->yy++;
-            break;
-        case 2: // Abajo
-            head->y--;
-            head->yy--;
-            break;
-        case 3: // Izquierda
-            head->x--;
-            head->xx--;
-            break;
-        case 4: // Derecha
-            head->x++;
-            head->xx++;
-            break;
-    }
-
-    // Mover el resto del cuerpo
-    temp = head->next;
-    while (temp != NULL) {
-        unsigned short temp_x = temp->x;
-        unsigned short temp_y = temp->y;
-        unsigned short temp_xx = temp->xx;
-        unsigned short temp_yy = temp->yy;
-
-        temp->x = prev_x;
-        temp->y = prev_y;
-        temp->xx = prev_xx;
-        temp->yy = prev_yy;
-
-        prev_x = temp_x;
-        prev_y = temp_y;
-        prev_xx = temp_xx;
-        prev_yy = temp_yy;
-
-        temp = temp->next;
-    }
-
-    // Dibujar la serpiente en sus nuevas posiciones
-    temp = head;
-    spawn_snake_part(temp->x, temp->y, temp->xx, temp->yy, ro);
-
-}
-
-void generate_snake(){
-    Snake *gen = head;
-    while(gen != NULL){
-        spawn_snake_part(gen->x,gen->y,gen->xx,gen->yy,ro);
-        gen = gen -> next;
-    }
-    return;
-
-}
-
-void spawn_apple(){
-    ax = rand() % (max_x + 1);
-    ay = rand() % (max_y + 1);
-    
-    printf("%d ", ax); 
-    printf("%d ", ay);
-    
-    unsigned int apple_c = 0x0000ff00;
-    set_pixel(ax, ay, apple_c);
-    set_pixel(ax, ay+1, apple_c);
-    set_pixel(ax+1, ay, apple_c);
-    set_pixel(ax+1, ay+1, apple_c);
-}
-
 void set_pixel(unsigned int x, unsigned int y, unsigned int color) {
+    if (x > max_x || y > max_y) return;
+
     unsigned int *led_base = LED_MATRIX_0_BASE;
-    unsigned int *address; 
-    unsigned int offset;
-    
-     offset = x + (24-y) * LED_MATRIX_0_WIDTH;    //PARA QUE EL ORIGEN SEA LA ESQUINA DE ABAJO
-    //offset = y + x * LED_MATRIX_0_WIDTH;
-    address = led_base + offset;
-    *(address) = color;
+    unsigned int offset = x + (24 - y) * LED_MATRIX_0_WIDTH;
+    unsigned int *address = led_base + offset;
+    *address = color;
 }
 
 void delay() {
-    for (volatile int i = 0; i < 500; i++) {}
-}
-
-void spawn_square(unsigned int x, unsigned int y, unsigned int color){
-    set_pixel(x, y, color);
-    set_pixel(x, y+1, color);
-    set_pixel(x+1, y, color);
-    set_pixel(x+1, y+1, color);
-}
-void movimiento(unsigned int mov){
-    switch(mov){
-        case 1://arriba
-            spawn_square(x,y,0);
-            y += 1;
-            spawn_square(x,y,ro);
-            break;
-        case 2://abajo
-            spawn_square(x,y,0);
-            y -= 1;
-            spawn_square(x,y,ro);
-            break;
-        case 3://izq
-            spawn_square(x,y,0);
-            x -= 1;
-            spawn_square(x,y,ro);
-            break;
-        case 4://derecha
-            spawn_square(x,y,0);
-            x += 1;
-            spawn_square(x,y,ro);
-            break;
-    }
+    for (volatile int i = 0; i < 1100; i++) {}
 }
