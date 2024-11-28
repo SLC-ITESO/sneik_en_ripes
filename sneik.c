1a70ba36bb0ccab5774de1ee6b014a73c0ff9b00
SantiL <ismael.lopez@iteso.mx>
2024-11-27 20:37:48 -0600

    Pantalla de Muerte
    Cuando muere aparece una pantalla de muerte. Además se corrigió que la manzana aparece dentro de la serpiente, o cortada afuera del escenario. También el bug que una parte de la serpiente puede estar afuera del tablero.
    
    Cosas que faltan:
    Comentariar más el codigo

--- sneik.c
+++ sneik.c
@@ -13,11 +13,16 @@ void spawn_snake();
 void grow_snake();
 void move_snake();
 void generate_snake();
+int check_collision();
+
+void toda_la_pantalla_de_un_color(unsigned int color);
+void ayanami_rei();
 
 volatile unsigned int *d_pad_u = D_PAD_0_UP;
 volatile unsigned int *d_pad_d = D_PAD_0_DOWN;
 volatile unsigned int *d_pad_l = D_PAD_0_LEFT;
 volatile unsigned int *d_pad_r = D_PAD_0_RIGHT;
+volatile unsigned int *switch_base = SWITCHES_0_BASE;
 
 // cosos del tablero
 unsigned short max_x = LED_MATRIX_0_WIDTH - 1;
@@ -45,17 +50,15 @@ unsigned int ro = 0x00ff0000;
 unsigned int ve = 0x0000ff00;
 
 void main() {
-    unsigned int mov = 4; // 1 = , 2 = down, 3 = left, 4 = right
-    
-    // spawnea todo
-    spawn_apple(ve);
+    // Inicializar el juego
     spawn_snake();
-    
+    spawn_apple(ve);
+
     while (1) {
-        //spawn_apple(ve);
+        // Generar la serpiente
         generate_snake();
-        
-        // DPAD
+
+        // Control del DPAD
         if (*d_pad_u == 1 && snake[0].dir != 2) {
             snake[0].dir = 1;
         }
@@ -69,8 +72,16 @@ void main() {
             snake[0].dir = 4;
         }
 
-        //Movimiento de la culebra
+        // Movimiento de la serpiente
         move_snake();
+        if (check_collision() || *switch_base == 1) {
+            pantalla_color(0x00abeeff);
+
+            while (*switch_base != 1) {}
+            while (*switch_base != 0) {}
+
+            ayanami_rei();
+        }
         come_apple();
         delay();
     }
@@ -79,8 +90,8 @@ void main() {
 void spawn_snake() {
     if (snake_size < MAX_SNAKE_PARTS) {
         SnakePart *head = &snake[0];
-        snake[0].x = rand() % (max_x);
-        snake[0].y = rand() % (max_y);
+        snake[0].x = rand() % 10;
+        snake[0].y = rand() % (max_y - 1) + 1;
         snake[0].xx = snake[0].x - 1;
         snake[0].yy = snake[0].y - 1;
         snake[0].dir = 4; // Comienza moviendose a la derecha
@@ -143,15 +154,51 @@ void move_snake() {
             head->xx++;
             break;
     }
+    
     //Dibuja la nueva cabeza
     spawn_snake_part(head->x, head->y, head->xx, head->yy, ro);
 }
 
+void ayanami_rei(){
+
+    snake_size = 2;
+    spawn_snake();
+    pantalla_color(0x00000000); 
+    spawn_apple(ve);
+}
+
+void pantalla_color(unsigned int color) {
+    for (unsigned int y = 0; y <= max_y; y++) {
+        for (unsigned int x = 0; x <= max_x; x++) {
+            set_pixel(x, y, color);
+        }
+    }
+}
 void generate_snake() {
     for (int i = 0; i < snake_size; i++) {
         spawn_snake_part(snake[i].x, snake[i].y, snake[i].xx, snake[i].yy, ro);
     }
 }
+
+int check_collision() {
+    // Verifica si la cabeza está fuera de los límites
+    SnakePart *head = &snake[0];
+    if (head->x > max_x || head->y > max_y || head->x < 0 || head->y < 0 ||
+        head->xx > max_x || head->yy > max_y || head->xx < 0 || head->yy < 0) {
+        return 1; // Colisión con un borde
+    }
+
+    // Verifica si la cabeza choca con el cuerpo
+    for (int i = 1; i < snake_size; i++) {
+        if ((head->x == snake[i].x && head->y == snake[i].y) || 
+            (head->xx == snake[i].xx && head->yy == snake[i].yy)) {
+            return 1; // Colisión consigo misma
+        }
+    }
+
+    return 0; // No hay colisión
+}
+
 void despawn_apple(){
     set_pixel(ax, ay, 0);
     set_pixel(axx, ay, 0);
@@ -159,15 +206,39 @@ void despawn_apple(){
     set_pixel(axx, ayy, 0);
 }
 void spawn_apple(unsigned int color_m) {
-    ax = rand() % (max_x - 1);
-    ay = rand() % (max_y - 1);
-    axx = ax - 1;
-    ayy = ay -1;
-    
+int pos_valida = 0;
+
+    while (!pos_valida) {
+        ax = rand() % (max_x - 1) + 1;
+        ay = rand() % (max_y - 1) + 1;
+        axx = ax - 1;
+        ayy = ay - 1;
+
+        pos_valida = 1;
+
+        // Verifica que las coordenadas no son las de la serpiente y su cuerpo
+        for (int i = 0; i < snake_size; i++) {
+            if (
+                (snake[i].x == ax && snake[i].y == ay) ||
+                (snake[i].x == axx && snake[i].y == ay) ||
+                (snake[i].x == ax && snake[i].y == ayy) ||
+                (snake[i].x == axx && snake[i].y == ayy) ||
+                (snake[i].xx == ax && snake[i].yy == ay) ||
+                (snake[i].xx == axx && snake[i].yy == ay) ||
+                (snake[i].xx == ax && snake[i].yy == ayy) ||
+                (snake[i].xx == axx && snake[i].yy == ayy)
+            ) {
+                pos_valida = 0;
+                break;
+            }
+        }
+    }
+
     set_pixel(ax, ay, color_m);
     set_pixel(axx, ay, color_m);
     set_pixel(ax, ayy, color_m);
     set_pixel(axx, ayy, color_m);
+
 }
 
 void spawn_snake_part(unsigned short x, unsigned short y, unsigned short xx, unsigned short yy, unsigned int color) {
@@ -187,5 +258,5 @@ void set_pixel(unsigned int x, unsigned int y, unsigned int color) {
 }
 
 void delay() {
-    for (volatile int i = 0; i < 500; i++) {}
+    for (volatile int i = 0; i < 1000; i++) {}
 }
